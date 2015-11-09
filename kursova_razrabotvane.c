49806f05e6ebc4b8334ad183dd8c3147a8c79be5
YordankaTumbakova <danita_15@abv.bg>
2015-11-09 14:44:35 +0200

    Update kursova_razrabotvane.c

--- kursova_razrabotvane.c
+++ kursova_razrabotvane.c
@@ -3,7 +3,13 @@
 //3. Broi cifri v komentari
 //4. Da se opredeli nai-dylgiq komentaren tekst ( v broi simvoli i nomer na nachalen red)
 #include<stdio.h>
+#include<stdlib.h>
+
 void chosenFile(int* chooseFile);
+void countBrackets(*fp);
+void countNumbersInCommnents(*fp);
+void longestTextComment(*fp);
+
 
 int main()
 {
@@ -12,66 +18,71 @@ int main()
 	int chooseFunction=0;
 	do
 	{
-	   	printf("Choose function from 1 to 5(5 is Exit):");	
+	   	printf("Choose function from 1 to 4(4 is Exit):");	
 		scanf(" %d", &chooseFunction); 
 	}
-		while(chooseFunction<1 || chooseFunction>5);
+		while(chooseFunction<1 || chooseFunction>4);
  
  	switch(chooseFunction)
 	{
 	    case 1:
-	    printf("Finding the number of all {}\n"); 
-	    chosenFile(chooseFile);
-	    
-	    
+	    	printf("Finding the number of all {}\n"); 
+	        chosenFile(chooseFile);
+	        countBrackets(fp);
 	    break;
 	    
 	    case 2:
-	    printf("Counting the numbers in the comments\n");
-	    chosenFile(chooseFile);
-	    
+	        printf("Counting the numbers in the comments\n");
+	        chosenFile(chooseFile);
+	        countNumbersInCommnents(fp);
 	    break;
+	    
 	    case 3:
-	    printf("The longest comment row (number of symbols and the number of the first row\n");
-	    chosenFile(chooseFile);
+	        printf("The longest comment row (number of symbols and the number of the first row\n");
+	        chosenFile(chooseFile);
+	        longestTextComment(fp);
+	    break;
 	    
-	    
+	    case 4:
+	        printf("Exit!\n");
+	        exit(0);
+	    break;
 	}
 	   
 	
 	
-return 0;
+    return 0;
 }
-void chosenFile(int* chooseFile)
-{
-    do
-	{
-		printf("Choose file from 1 to 4:");	
-		scanf(" %d", &chooseFile);
-	}
-	while(chooseFile<1 || chooseFile>4);
-	
-    switch(chooseFile)
-	{
-		case 1: 
-		printf("You've chosen File 1");  
-		fp = fopen("/file1.c", "r");
-		break;
+    void chosenFile(int* chooseFile, *fp)
+    {
+      do
+    	{
+    		printf("Choose file from 1 to 4:");	
+    		scanf(" %d", &chooseFile);
+	    }
+    	while(chooseFile<1 || chooseFile>4);
+    	
+      switch(chooseFile)
+    	{
+    		case 1: 
+    			printf("You've chosen File 1");  
+    	    		*fp = fopen("/file1.c", "r");
+    		break;
 		
-		case 2: 
-		printf("You've chosen File 2");   
-		fp = fopen("/file2.c", "r");
-		break;
+	    	case 2: 
+	    		printf("You've chosen File 2");   
+	    		*fp = fopen("/file2.c", "r");
+	    	break;
 		
-		case 3: 
-		printf("You've chosen File 3");  
-		fp = fopen("/file3.c", "r");
+	    	case 3: 
+			printf("You've chosen File 3");  
+			*fp = fopen("/file3.c", "r");
 		break;
 		
 		case 4: 
-		printf("You've chosen File 4");   
-		fp = fopen("/file4.c", "r");
+			printf("You've chosen File 4");   
+			*fp = fopen("/file4.c", "r");
 		break;
 	
-	}
-}
+	    }
+    }
