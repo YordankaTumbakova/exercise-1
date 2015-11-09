5db80e305b9ba86667f69603ef9f823bee07a7c3
YordankaTumbakova <danita_15@abv.bg>
2015-11-09 14:17:36 +0200

    Create kursova_razrabotvane.c

---
+++ kursova_razrabotvane.c
@@ -0,0 +1,77 @@
+//1.Izbirane na fail s vyvejdane na poredniq mu nomer ot daden spisyk
+//2. Broi {}
+//3. Broi cifri v komentari
+//4. Da se opredeli nai-dylgiq komentaren tekst ( v broi simvoli i nomer na nachalen red)
+#include<stdio.h>
+void chosenFile(int* chooseFile);
+
+int main()
+{
+	FILE *fp=NULL;
+	int chooseFile=0;
+	int chooseFunction=0;
+	do
+	{
+	   	printf("Choose function from 1 to 5(5 is Exit):");	
+		scanf(" %d", &chooseFunction); 
+	}
+		while(chooseFunction<1 || chooseFunction>5);
+ 
+ 	switch(chooseFunction)
+	{
+	    case 1:
+	    printf("Finding the number of all {}\n"); 
+	    chosenFile(chooseFile);
+	    
+	    
+	    break;
+	    
+	    case 2:
+	    printf("Counting the numbers in the comments\n");
+	    chosenFile(chooseFile);
+	    
+	    break;
+	    case 3:
+	    printf("The longest comment row (number of symbols and the number of the first row\n");
+	    chosenFile(chooseFile);
+	    
+	    
+	}
+	   
+	
+	
+return 0;
+}
+void chosenFile(int* chooseFile)
+{
+    do
+	{
+		printf("Choose file from 1 to 4:");	
+		scanf(" %d", &chooseFile);
+	}
+	while(chooseFile<1 || chooseFile>4);
+	
+    switch(chooseFile)
+	{
+		case 1: 
+		printf("You've chosen File 1");  
+		fp = fopen("/file1.c", "r");
+		break;
+		
+		case 2: 
+		printf("You've chosen File 2");   
+		fp = fopen("/file2.c", "r");
+		break;
+		
+		case 3: 
+		printf("You've chosen File 3");  
+		fp = fopen("/file3.c", "r");
+		break;
+		
+		case 4: 
+		printf("You've chosen File 4");   
+		fp = fopen("/file4.c", "r");
+		break;
+	
+	}
+}
